2d35afa7452b1e1a208d38ce0155ac0fd4d24a70
AlvesRicardo92 <ricardobalves3@gmail.com>
2023-06-13 16:45:32 -0300

    #largura menu2

--- menu2.php
+++ menu2.php
@@ -1,65 +1,67 @@
-<nav class="navbar navbar-expand-xxl bg-dark navbar-dark">
-    <a class="navbar-brand" href="#">Sec. de Transportes</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav" id="menu">
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="index3.php" class="nav-link align-middle px-0">
-                    <i class="bi bi-house"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Início</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="si.php" class="nav-link align-middle px-0">
-                    <i class="bi bi-clipboard2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">SI</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="semaforica.php" class="nav-link align-middle px-0">
-                    <i class="bi bi-stoplights"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Semafórica</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="carros.php" class="nav-link align-middle px-0">
-                    <i class="bi bi-speedometer2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Carros</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="projeto.php" class="nav-link align-middle px-0">
-                    <i class="bi bi-cone-striped"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Projeto</span>
-                </a>
-            </li> 
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-file-earmark-check"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Oficios</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-globe2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Certidões</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-play-btn"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">O.S.</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-recycle"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Cont. de Implantações</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-pin-map-fill"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Radar e Estatistica</span>
-                </a>
-            </li>
-            <li class="nav-item" style="margin-right:5px;margin-left:5px">
-                <a href="" class="nav-link align-middle px-0">
-                    <i class="bi bi-pencil"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Relatórios</span>
-                </a>
-            </li>                           
-        </ul>
-    </div>  
-</nav>
+<div class="container col-md-12 mb-3">
+    <nav class="navbar navbar-expand-xxl bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">Sec. de Transportes</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav" id="menu">
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="index3.php" class="nav-link align-middle px-0">
+                        <i class="bi bi-house"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Início</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="si.php" class="nav-link align-middle px-0">
+                        <i class="bi bi-clipboard2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">SI</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="semaforica.php" class="nav-link align-middle px-0">
+                        <i class="bi bi-stoplights"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Semafórica</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="carros.php" class="nav-link align-middle px-0">
+                        <i class="bi bi-speedometer2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Carros</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="projeto.php" class="nav-link align-middle px-0">
+                        <i class="bi bi-cone-striped"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Projeto</span>
+                    </a>
+                </li> 
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-file-earmark-check"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Oficios</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-globe2"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Certidões</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-play-btn"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">O.S.</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-recycle"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Cont. de Implantações</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-pin-map-fill"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Radar e Estatistica</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="margin-right:5px;margin-left:5px">
+                    <a href="" class="nav-link align-middle px-0">
+                        <i class="bi bi-pencil"></i>&nbsp;&nbsp;<span class="ms-1 d-sm-inline">Relatórios</span>
+                    </a>
+                </li>                           
+            </ul>
+        </div>  
+    </nav>
+</div>
