29c48b15e6773c6160294b9b5ac1949811bd7fb1
frsartori0507 <135068697+frsartori0507@users.noreply.github.com>
2023-06-16 16:18:16 -0300

    Revert "Merge branch 'main' of https://github.com/AlvesRicardo92/Sistema-Sec.-de-Transportes"
    This reverts commit 865b82c3821da1ebc3bb28c187c5ef343db16e9a, reversing
    changes made to e178c495ada55ad6f90188dc63ddebf627741d45.

--- js/scripts.js
+++ js/scripts.js
@@ -567,7 +567,7 @@ function buscarEndereco(enderecoDigitado){
         });
     }
 }
-function buscarSI(dados){
+function buscarDiaria(dados){
 	var tipoPesquisa;
     if(document.getElementById("numeroPesquisa").checked){
         tipoPesquisa="numero";
@@ -586,7 +586,7 @@ function buscarSI(dados){
     }
     else{
         $.ajax({
-            url: 'buscarSI.php',
+            url: 'buscarDiaria.php',
             async:false,
             type: 'POST',
             data: {tipoPesquisa: tipoPesquisa,
@@ -597,7 +597,7 @@ function buscarSI(dados){
             },
             success: function (resultado) {
                 if (resultado!="Não encontrado"){
-                    var tabelaEndereco = document.getElementById('tabelaResultadoSI').getElementsByTagName('tbody')[0];
+                    var tabelaEndereco = document.getElementById('tabelaResultadoDiaria').getElementsByTagName('tbody')[0];
                     tabelaEndereco.innerHTML=resultado;
                 }
                 else{
@@ -775,20 +775,6 @@ function logar(){
 function imprimirSI(){
     window.location.href = "impressaoSI.php";
 }
-function desbloquearAlteracaoSI(){
-    /*
-    Fazer procedimento para desbloquear os campos para o usuário alterar o conteúda da SI, quando for a mesma pessoa que criou
-    Fazer procedimento para quando clicar no salvar, atualizar ao invés de salvar denovo. 
-    Algo como 
-        if(siNumero!=""){
-            Fazer Update
-        }
-        else{
-            Fazer insert
-        }
-    
-    */
-}
 $(document).ready(function() {
     $(document).on('click', '.excluir', function() {
         $(this).closest('tr').remove();
@@ -801,11 +787,7 @@ $(document).ready(function() {
             primeiroTD = $(this).find('td:first');
             segundoTD = $(this).find('td:eq(1)');
           });
-        $('#logradouro').val(primeiroTD.text());
-        $('#bairro').val(segundoTD.text());
-        $("#numEndereco").removeAttr("disabled");
-        $('#fecharModal').click();
-        /*if ($("#chamada").text()==="L1"){
+        if ($("#chamada").text()==="L1"){
             $('#logradouro').val(primeiroTD.text());
             $('#bairro').val(segundoTD.text());
             $("#numEndereco").removeAttr("disabled");
@@ -814,82 +796,6 @@ $(document).ready(function() {
         else{
             $('#logradouroCruzamento').val(primeiroTD.text());
             $('#fecharModal').click();
-        }*/
-    });
-    $(document).on('click', '.escolherSI', function() {
-        var linhaSelecionada = $(this).closest('tr');
-        var primeiroTD = "";
-        var siNumeroAno;
-        console.log("linha selecionada "+linhaSelecionada);
-        $.each(linhaSelecionada , function() {
-            primeiroTD = $(this).find('td:first');
-            siNumeroAno = primeiroTD.text().split("/");
-            //segundoTD = $(this).find('td:eq(1)');
-        });
-
-        $.ajax({
-            url: 'trazInfoSI.php',
-            async:false,
-            type: 'POST',
-            data: {idSI:siNumeroAno[0],
-                   anoSI:siNumeroAno[1]},
-            dataType:'text',
-            done: function () {
-                alert("feito");
-            },
-            success: function (resultado) {
-                //console.log("resultado " + resultado);
-                var retorno = resultado.split('|');
-                //console.log("retorno " + retorno);
-                if (Array.isArray(retorno)){
-                    if(retorno[0]>0){
-                        $('#siNumero').val(retorno[0]);
-                        $('#siData').val(retorno[1].substring(0,10));
-                        $('#resp01 option:contains("'+$.trim(retorno[3])+'")').prop('selected', true);
-                        $('#resp02 option:contains("'+$.trim(retorno[4])+'")').prop('selected', true);
-                        $('#destino option:contains("'+$.trim(retorno[5])+'")').prop('selected', true);
-                        $('#solicitante').val(retorno[6]);
-                        $('#assunto').val(retorno[7]);
-                        $('#logradouro').val(retorno[8]);
-                        $('#numEndereco').val(retorno[9]);
-                        $('#bairro').val(retorno[10]);
-                        $('#obs').val(retorno[11]);
-                        $('#anotacoes').val(retorno[12]);
-                        if(retorno[2]=="URGENCIAR"){
-                            document.getElementById("urgente").checked=true;
-                        }
-                        else if(retorno[2]=="PRIORIZAR"){
-                            document.getElementById("priorizar").checked=true;
-                        }
-                        else if(retorno[2]=="NORMAL"){
-                            document.getElementById("normal").checked=true;
-                        }
-                        $('#fecharModalPesquisa').click();
-                        if(retorno[13]===document.getElementById("iniciais").innerText){
-                            //desbloquearAlteracaoSI();
-                            alert("Mesmo usuário da criação");
-                        }
-                        else{
-                            alert("Não é o mesmo usuário da criação");
-                        }
-                    }
-                    else{
-                        console.log("primeiro item não é >0 = " + retorno);
-                        alert("Erro ao pesquisar a SI. Verifique o console");
-                    }
-                }
-                else{
-                    console.log("retorno não é um array = " + retorno);
-                    alert("Erro ao pesquisar a SI. Verifique o console");
-                }
-            },
-            fail: function(){
-                alert("falha");
-            },
-            error: function(){
-                alert("error");
-            }
-        });
-        $('#fecharModalPesquisa').click();
+        }
     });
 });
