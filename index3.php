98846f8809a38e67ae32e5f5528e763098287d18
AlvesRicardo92 <ricardobalves3@gmail.com>
2023-06-20 14:50:05 -0300

    ajuste na qtde de slides e tamanho do carrossel

--- index3.php
+++ index3.php
@@ -40,34 +40,44 @@ $result -> free_result();
 									<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 									<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
 									<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+									<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
 								</div>
 								<div class="carousel-inner">
-									<div class="carousel-item active">
+									<div class="carousel-item active" style="height:300px">
 										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 										<div class="container">
-											<div class="carousel-caption text-start">
+											<div class="carousel-caption text-start"><!-- Alinhamento do conteúdo do slide à esquerda text-start -->
 												<h3>Entrega de IRPF</h3>
-												<p>Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.of the carousel.Some representative placeholder content for the first slide of the carousel.</p>
-												
+												<p>Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.Some representative placeholder content for the first slide of the carousel.of the carousel.Some representative placeholder content for the first slide of the carousel.</p>
 											</div>
 										</div>
 									</div>
-									<div class="carousel-item">
+									<div class="carousel-item" style="height:300px">
 										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 										<div class="container">
-											<div class="carousel-caption">
+											<div class="carousel-caption"> <!-- Alinhamento do conteúdo do slide centralizado sem qualquer classe text-xxx -->
 												<h1>Another example headline.</h1>
 												<p>Some representative placeholder content for the second slide of the carousel.</p>
 												<p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
 											</div>
 										</div>
 									</div>
-									<div class="carousel-item">
+									<div class="carousel-item" style="height:300px">
+										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+										<div class="container">
+											<div class="carousel-caption text-end"><!-- Alinhamento do conteúdo do slide à direita text-end -->
+												<h1>One more for good measure.</h1>
+												<p>Some representative placeholder content for the third slide of this carousel.</p>
+												<p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+											</div>
+										</div>
+									</div>
+									<div class="carousel-item" style="height:300px">
 										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 										<div class="container">
 											<div class="carousel-caption text-end">
-												<h1>One more for good measure.</h1>
-												<p>Some representative placeholder content for the third slide of this carousel.</p>
+												<h1>quarto slide</h1>
+												<p>Some representative placeholder content for the third slide of this carousel.Some representative placeholder content for the third slide of this carousel.Some representative placeholder content for the third slide of this carousel.</p>
 												<p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
 											</div>
 										</div>
