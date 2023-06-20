8e5e55988cfe99322627675f2c666fddbddca38a
AlvesRicardo92 <ricardobalves3@gmail.com>
2023-06-20 15:01:04 -0300

    ajuste no carrossel

--- index3.php
+++ index3.php
@@ -44,7 +44,7 @@ $result -> free_result();
 								</div>
 								<div class="carousel-inner">
 									<div class="carousel-item active" style="height:300px">
-										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#333"/></svg>
 										<div class="container">
 											<div class="carousel-caption text-justify"><!-- Alinhamento do conteúdo do slide à esquerda text-start, justificado é text-justify -->
 												<h3>Entrega de IRPF</h3>
@@ -53,7 +53,7 @@ $result -> free_result();
 										</div>
 									</div>
 									<div class="carousel-item" style="height:300px">
-										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#555"/></svg>
 										<div class="container">
 											<div class="carousel-caption"> <!-- Alinhamento do conteúdo do slide centralizado sem qualquer classe text-xxx -->
 												<h1>Another example headline.</h1>
@@ -73,7 +73,7 @@ $result -> free_result();
 										</div>
 									</div>
 									<div class="carousel-item" style="height:300px">
-										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+										<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#af1"/></svg>
 										<div class="container">
 											<div class="carousel-caption text-end">
 												<h1>quarto slide</h1>
